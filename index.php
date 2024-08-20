352cf46153e957589dbf2bc0c9c7cedc750cb5a1
Jet Wolf <jetwolfv1@gmail.com>
2024-08-20 00:55:15 -0600

    first tests

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>The JW RB DB</title>
+  </head>
+  <body>
+    <iframe name="connect" src="connect.php" width="100%" frameborder="0"></iframe>
+  </body>
+</html>
