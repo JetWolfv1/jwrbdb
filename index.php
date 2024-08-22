16434b162a29b1265df8aed7bf68dcc22d2e20f0
NikPurvis <nikpurvis@gmail.com>
2024-08-22 10:31:53 -0600

    Host to DigitalOcean IP.

--- index.php
+++ index.php
@@ -3,9 +3,10 @@ $user = "example_user";
 $password = "reihinostits";
 $database = "example_database";
 $table = "todo_list";
+$host = "64.23.150.91";
 
 try {
-  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+  $db = new PDO("mysql:host=$host;dbname=$database", $user, $password);
   echo "<h2>TODO</h2><ol>";
   foreach($db->query("SELECT content FROM $table") as $row) {
     echo "<li>" . $row['content'] . "</li>";
