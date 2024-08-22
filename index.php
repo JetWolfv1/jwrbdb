43950b9a6dfd708958ad9ae3954fb89fec4ef709
NikPurvis <nikpurvis@gmail.com>
2024-08-22 13:13:54 -0600

    Return to localhost after some serverside adjustments.

--- index.php
+++ index.php
@@ -3,10 +3,9 @@ $user = "example_user";
 $password = "reihinostits";
 $database = "example_database";
 $table = "todo_list";
-$host = "http://64.23.150.91:80";
 
 try {
-  $db = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
   echo "<h2>TODO</h2><ol>";
   foreach($db->query("SELECT content FROM $table") as $row) {
     echo "<li>" . $row['content'] . "</li>";
