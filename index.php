4dfc83ee151be508be8272271606483966fe50bd
NikPurvis <nikpurvis@gmail.com>
2024-08-22 19:33:02 -0600

    try again

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
   $database = "jwrbdb";
   $table = "songs";
 
+  echo "<h5>Working on it!</h5>";
+
   try {
     $db = new PDO("mysql:host=$host;dbname=$database", $user, $password);
     echo "<h2>Songs</h2><ol>";
@@ -15,4 +17,5 @@
   } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br />";
     die();
-}
+  }
+?>
