a2be03a940687ef932f9c2437ca2a1801292295f
NikPurvis <nikpurvis@gmail.com>
2024-08-22 19:23:08 -0600

    Host change to ip vs localhost

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
+  $host = "127.0.0.1"
   $user = "breweb-client";
   $password = "reihinost1t$";
   $database = "jwrbdb";
   $table = "songs";
 
   try {
-    $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+    $db = new PDO("mysql:host=$host;dbname=$database", $user, $password);
     echo "<h2>Songs</h2><ol>";
     foreach($db->query("SELECT title FROM $table") as $row) {
       echo "<li>" . $row['title'] . "</li>";
