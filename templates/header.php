61fe9ef3cc8fdeeac5f7180b5214e589c52362e5
NikPurvis <nikpurvis@gmail.com>
2024-08-23 19:01:47 -0600

    Adding bootstrap

--- templates/header.php
+++ templates/header.php
@@ -6,20 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <title>JWRBDB</title>
-
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
 
   <body>
-    <p><b><a href="index.php">Home</a></b></p>
-    <p>Sort by:
-      <b><a href="title.php">Title</a></b> ||
-      <b><a href="artist.php">Artist</a></b> ||
-      <!-- <b><a href="year.php">Year</a></b> || -->
-      <b><a href="genre.php">Genre</a></b></p>
-    <p>Filter by:
-      <b><a href="twopart.php">2-part vocals</a></b> ||
-      <b><a href="threepart.php">3-part vocals</a></b></p>
+    <div class="container">
+
+      <p><b><a href="index.php">Home</a></b></p>
+        <p>Sort by:
+          <b><a href="title.php">Title</a></b> ||
+          <b><a href="artist.php">Artist</a></b> ||
+          <!-- <b><a href="year.php">Year</a></b> || -->
+          <b><a href="genre.php">Genre</a></b></p>
+        <p>Filter by:
+          <b><a href="twopart.php">2-part vocals</a></b> ||
+          <b><a href="threepart.php">3-part vocals</a></b></p>
 
 
   <!-- <h1>JWRBDB</h1> -->
