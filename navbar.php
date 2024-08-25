21858ecc02727ef9bc2c417001041d2fa7b11865
Jet Wolf <jetwolfv1@gmail.com>
2024-08-25 12:15:52 -0600

    Removed navbar collapse for mobile

--- navbar.php
+++ navbar.php
@@ -3,7 +3,7 @@
   
     <a href="index.php" class="navbar-brand">Jill's 40th? Party.</a>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="" id="navbarSupportedContent">
       <ul class="nav nav-tabs">
         <li class="nav-item active">
           <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
